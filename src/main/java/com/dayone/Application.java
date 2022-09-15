b76f14465dead744dc7081678bbc6978cc8b8bd9
DESKTOP-R7ANO7G\lee <nicet.shlee@gmail.com>
2022-09-16 00:46:33 +0900

    no message

--- src/main/java/com/dayone/Application.java
+++ src/main/java/com/dayone/Application.java
@@ -16,32 +16,14 @@ public class Application {
     public static void main(String[] args) {
 //        SpringApplication.run(Application.class, args);
 
-        try {
-            Connection connection = Jsoup.connect("https://finance.yahoo.com/quote/COKE/history?period1=99100800&period2=1663113600&interval=1mo&filter=history&frequency=1mo&includeAdjustedClose=true");
-            Document document = connection.get();
+        String s = "hello my name is %s";
 
-            Elements eles = document.getElementsByAttributeValue("data-test", "historical-prices");
-            Element ele = eles.get(0); // table 전체
+        String[] names = {"green", "red", "blue"};
 
-            Element tbody = ele.children().get(1); //tbody 0은 thead 2는 tfoot
-            for (Element e : tbody.children()) {
-                String txt = e.text();
-                if (!txt.endsWith("Dividend")) {
-                    continue;
-                }
-//              txt형태 -> Jul 28, 2022 0.25 Dividend
-                String[] splits = txt.split(" ");
-                String month = splits[0];
-                int day = Integer.valueOf(splits[1].replace(",", ""));
-                int year = Integer.valueOf(splits[2]);
-                String dividend = splits[3];
-
-                System.out.println(year + "/" + month + "/" + day + " -> " + dividend);
-            }
-            
-        }catch (IOException e) {
-            e.printStackTrace();
+        for (String name : names) {
+            System.out.println(String.format(s, name));
         }
+
 
     }
 }
---
+++ src/main/java/com/dayone/scraper/YahooFinanceScraper.java
@@ -0,0 +1,48 @@
+package com.dayone.scraper;
+
+import com.dayone.model.Company;
+import com.dayone.model.ScrapedResult;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class YahooFinanceScraper {
+
+    private static final String URL = "https://finance.yahoo.com/quote/COKE/history?period1=99100800&period2=1663113600&interval=1mo&filter=history&frequency=1mo&includeAdjustedClose=true";
+
+    public ScrapedResult scrap(Company company) {
+        
+        try {
+            Connection connection = Jsoup.connect(URL);
+            Document document = connection.get();
+
+            Elements eles = document.getElementsByAttributeValue("data-test", "historical-prices");
+            Element ele = eles.get(0); // table 전체
+
+            Element tbody = ele.children().get(1); //tbody 0은 thead 2는 tfoot
+            for (Element e : tbody.children()) {
+                String txt = e.text();
+                if (!txt.endsWith("Dividend")) {
+                    continue;
+                }
+//              txt형태 -> Jul 28, 2022 0.25 Dividend
+                String[] splits = txt.split(" ");
+                String month = splits[0];
+                int day = Integer.valueOf(splits[1].replace(",", ""));
+                int year = Integer.valueOf(splits[2]);
+                String dividend = splits[3];
+
+                System.out.println(year + "/" + month + "/" + day + " -> " + dividend);
+            }
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
