301b3cb0e6ecaf97f8f17b0f8288922652b61d3c
DESKTOP-R7ANO7G\lee <nicet.shlee@gmail.com>
2022-09-16 02:50:32 +0900

    문자열 int로 변경위한 enum 추가 스크래핑 출력코드 연결 및 테스트

--- src/main/java/com/dayone/Application.java
+++ src/main/java/com/dayone/Application.java
@@ -1,5 +1,7 @@
 package com.dayone;
 
+import com.dayone.model.Company;
+import com.dayone.scraper.YahooFinanceScraper;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -15,15 +17,10 @@ public class Application {
 
     public static void main(String[] args) {
 //        SpringApplication.run(Application.class, args);
-//      String format 기능 설명
-        
-        String s = "hello my name is %s";
 
-        String[] names = {"green", "red", "blue"};
-
-        for (String name : names) {
-            System.out.println(String.format(s, name));
-        }
+        YahooFinanceScraper scraper = new YahooFinanceScraper();
+        var result = scraper.scrap(Company.builder().ticker("0").build());
+        System.out.println(result);
 
 
     }
--- src/main/java/com/dayone/scraper/YahooFinanceScraper.java
+++ src/main/java/com/dayone/scraper/YahooFinanceScraper.java
@@ -1,7 +1,9 @@
 package com.dayone.scraper;
 
 import com.dayone.model.Company;
+import com.dayone.model.Dividend;
 import com.dayone.model.ScrapedResult;
+import com.dayone.model.constants.Month;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -9,20 +11,30 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class YahooFinanceScraper {
 
-    private static final String URL = "https://finance.yahoo.com/quote/COKE/history?period1=99100800&period2=1663113600&interval=1mo&filter=history&frequency=1mo&includeAdjustedClose=true";
+    private static final String STATISTICS_URL = "https://finance.yahoo.com/quote/COKE/history?period1=%d&period2=%d&interval=1mo";
+    private static final long START_TIME = 86400;
 
     public ScrapedResult scrap(Company company) {
+        var scrapResult = new ScrapedResult();
+        scrapResult.setCompany(company);
         try {
-            Connection connection = Jsoup.connect(URL);
+            long now = System.currentTimeMillis() / 1000; //currentTimeMillis() 1970년 1월 1일부터 경과한 시간을 밀리세컨드로 표현한 수
+            String url = String.format(STATISTICS_URL, company.getTicker(), START_TIME ,now);
+            Connection connection = Jsoup.connect(url);
             Document document = connection.get();
 
-            Elements eles = document.getElementsByAttributeValue("data-test", "historical-prices");
-            Element ele = eles.get(0); // table 전체
+            Elements parsingDivs = document.getElementsByAttributeValue("data-test", "historical-prices");
+            Element tableEle = parsingDivs.get(0); // table 전체
 
-            Element tbody = ele.children().get(1); //tbody 0은 thead 2는 tfoot
+            Element tbody = tableEle.children().get(1); //tbody 0은 thead 2는 tfoot
+
+            List<Dividend> dividends = new ArrayList<>();
             for (Element e : tbody.children()) {
                 String txt = e.text();
                 if (!txt.endsWith("Dividend")) {
@@ -30,18 +42,29 @@ public class YahooFinanceScraper {
                 }
 //              txt형태 -> Jul 28, 2022 0.25 Dividend
                 String[] splits = txt.split(" ");
-                String month = splits[0];
+                int month = Month.strToNumber(splits[0]); //enum Month 클래스로 string을 int로 불러옴
                 int day = Integer.valueOf(splits[1].replace(",", ""));
                 int year = Integer.valueOf(splits[2]);
                 String dividend = splits[3];
 
-                System.out.println(year + "/" + month + "/" + day + " -> " + dividend);
+                if (month < 0) {
+                    throw new RuntimeException("Unexpected Month enum value -> " + splits[0]);
+                }
+
+                dividends.add(Dividend.builder()
+                        .date(LocalDateTime.of(year, month, day, 0, 0))
+                        .dividend(dividend)
+                        .build());
+
+//                System.out.println(year + "/" + month + "/" + day + " -> " + dividend);
+
             }
+            scrapResult.setDividendEntities(dividends);
 
         }catch (IOException e) {
             e.printStackTrace();
         }
 
-        return null;
+        return scrapResult;
     }
 }
