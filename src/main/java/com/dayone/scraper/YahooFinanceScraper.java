57627cc7ecf5dfc7760af3959bb5b536aa55bc72
DESKTOP-R7ANO7G\lee <nicet.shlee@gmail.com>
2022-09-16 15:28:29 +0900

    오타 수정

--- src/main/java/com/dayone/scraper/YahooFinanceScraper.java
+++ src/main/java/com/dayone/scraper/YahooFinanceScraper.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 public class YahooFinanceScraper {
 
-    private static final String STATISTICS_URL = "https://finance.yahoo.com/quote/COKE/history?period1=%d&period2=%d&interval=1mo";
+    private static final String STATISTICS_URL = "https://finance.yahoo.com/quote/%s/history?period1=%d&period2=%d&interval=1mo";
     private static final long START_TIME = 86400;
 
     public ScrapedResult scrap(Company company) {
@@ -25,6 +25,7 @@ public class YahooFinanceScraper {
         scrapResult.setCompany(company);
         try {
             long now = System.currentTimeMillis() / 1000; //currentTimeMillis() 1970년 1월 1일부터 경과한 시간을 밀리세컨드로 표현한 수
+
             String url = String.format(STATISTICS_URL, company.getTicker(), START_TIME ,now);
             Connection connection = Jsoup.connect(url);
             Document document = connection.get();
@@ -62,6 +63,7 @@ public class YahooFinanceScraper {
             scrapResult.setDividendEntities(dividends);
 
         }catch (IOException e) {
+            // TODO
             e.printStackTrace();
         }
 
