a9e0dabef36506be42172f88b048bd6dae65ce91
DESKTOP-R7ANO7G\lee <nicet.shlee@gmail.com>
2022-09-26 09:41:55 +0900

    스케줄러 코드 작성 // 쓰레드풀 생성 // 예시코드 작성

--- src/main/java/com/dayone/Application.java
+++ src/main/java/com/dayone/Application.java
@@ -10,10 +10,12 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.io.IOException;
 
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
