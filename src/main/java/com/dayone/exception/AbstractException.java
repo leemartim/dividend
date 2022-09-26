c6fdd8b2aada031587eb391137f4539736b50a07
DESKTOP-R7ANO7G\lee <nicet.shlee@gmail.com>
2022-09-26 15:35:52 +0900

    에러 메세지 처리 // 로그 남기기

--- src/main/java/com/dayone/exception/AbstractException.java
+++ src/main/java/com/dayone/exception/AbstractException.java
@@ -1,2 +1,7 @@
-package com.dayone.exception;public class AbstractException {
+package com.dayone.exception;
+
+public abstract class AbstractException extends RuntimeException {
+
+    abstract public int getStatusCode();
+    abstract public String getMessage();
 }
--- src/main/java/com/dayone/service/FinanceService.java
+++ src/main/java/com/dayone/service/FinanceService.java
@@ -1,5 +1,6 @@
 package com.dayone.service;
 
+import com.dayone.exception.impl.NoCompanyException;
 import com.dayone.model.Company;
 import com.dayone.model.Dividend;
 import com.dayone.model.ScrapedResult;
@@ -31,7 +32,7 @@ public class FinanceService {
         log.info("search company -> " + companyName);
         // 1. 회사명을 기준으로 회사 정보를 조회
         CompanyEntity company = this.companyRepository.findByName(companyName)
-                .orElseThrow(() -> new RuntimeException("존재하지 않는 회사명입니다.")); // 값이 없으면 예외를 배출
+                .orElseThrow(() -> new NoCompanyException()); // 값이 없으면 예외를 배출
         // 2. 조회된 회사의 ID로 배당금 정보를 조회
         List<DividendEntity> dividendEntityLies = this.dividendRepository.findAllByCompanyId(company.getId());
 
